23723b0810ddbd7fc972d571234b358e6bb6cd08
Lira7682 <a.23308051281191@cbtis128.edu.mx>
2024-09-27 16:55:23 -0600

    Add files via upload

---
+++ Lira_Hernandez_Dayana_Yamileth_1191_3W/Lira_Hernandez_Dayana_Yamileth_1191_3W.py
@@ -0,0 +1,15 @@
+
+print (" ") #imprime un espacio a agregar
+print ("Lira Hernandez Dayana Yamileth, numero ctrl ´1191´, 3W")
+#imprime datos del realizador
+print (" ") #imprime un espacio a agregar
+a = (input("Ingresa apellido paterno")) #declara valor de variable como nombre
+b = (input("Ingresa apellido materno")) #declara valor de variable como nombre
+c = (input("Ingresa primer nombre")) #declara valor de variable como nombre
+d = (input("Ingresa segundo nombre")) #declara valor de variable como nombre
+e = a + " " + b + " " + c + " " + d #concatenacion de las variables 
+print (" ")
+print (e.upper()) #imprime la concatenacion realizada
+print (" ") #imprime un espacio a agregar 
+
+
